801e9eeb97c71ecc1d865de1d0c915358f0b2339
mcarrenoreyes <m.carrenoreyes@uandresbello.edu>
2021-05-15 15:37:17 -0400

    se añade validación de datos

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Su contraseña ha sido cambiada.',
+    'sent' => 'Le hemos enviado un correo para cambiar la contraseña.',
+    'throttled' => 'Porfavor espere antres de volver a intentar.',
+    'token' => 'El token de la contraseña es inválido.',
+    'user' => "No se logró encontrar a un usuario con esta dirección de correo.",
 
 ];
